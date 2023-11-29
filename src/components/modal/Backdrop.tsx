76c3c9baeb30a5ed17260feeffd246821546712d
Pro <pro@MacBook-Pro-de-Pro.local>
2023-11-29 16:35:42 -0300

    refactor(Backdrop): Added classes to identify elements from test suite

--- src/components/modal/Backdrop.tsx
+++ src/components/modal/Backdrop.tsx
@@ -10,7 +10,7 @@ export default function Backdrop({children, onClick}: BackdropProps) {
     return (
         <motion.div
             onClick={onClick}
-            className="fixed z-50 h-screen w-screen top-0 left-0 bg-black/60 flex justify-center items-center"
+            className="backdrop fixed z-50 h-screen w-screen top-0 left-0 bg-black/60 flex justify-center items-center"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
